2073d431b934cc7230bca5c3f04d00c1f4ef487a
j9heiser <janine.heiser@sfgov.org>
2016-11-10 23:39:03 +0000

    added changes

--- pydev/Emailer.py
+++ pydev/Emailer.py
@@ -71,8 +71,8 @@ class Emailer():
         
         #normal emails, no attachment
         server = smtplib.SMTP(self._server, self._server_port)
-        server.starttls()
-        server.login(fromaddr, self._password)
+        #server.starttls()
+        #server.login(fromaddr, self._password)
         text = msg.as_string()
         server.sendmail(fromaddr, toaddr, text)
         server.quit()
