42c4c5d06b16f8ccb072c4eeacb21597117c6c46
j9recurses <janine.heiser@gmail.com>
2017-01-05 15:06:26 -0800

    uploaded attachments

--- pydev/ExistingFieldDefs.py
+++ pydev/ExistingFieldDefs.py
@@ -20,21 +20,25 @@ class ExistingFieldDefs:
         self._config_dir = configItems['config_dir']
         self._documented_fields_dir = configItems['documented_fields_dir']
         self._documented_fields_name_mapping = { 'datasetID': 'datasetid', 'columnID': 'columnid', 'Dataset Name': 'dataset_name', 'Field Name': 'field_name', 'Field Alias': 'field_alias', 'Definition': 'field_definition', 'Field Type Flag':'field_type_flag','Status': 'status', 'date_last_changed': 'date_last_changed', 'Data Type':'field_type'  }
-        self._documented_fields_list = [ 'columnid',  'datasetid', 'dataset_name',  'field_name',  'field_alias',  'field_definition', 'field_type_flag']
-        self._pdf_others_fields_list = ['datasetid', 'dataset_name', 'field_count', 'attachment_url']
+        #self._documented_fields_list = [ 'columnid',  'datasetid', 'dataset_name',  'field_name',  'field_alias',  'field_definition', 'field_type_flag']
+        self._documented_fields_matched = [ 'columnid',  'field_definition', 'status', 'date_last_changed' ]
+        self._documented_fields_unmatched = [ 'columnid',  'datasetid', 'dataset_name',  'field_name']
+        self._pdf_others_fields_list = ['datasetid', 'dataset_name', 'field_count', 'attachment_url', 'file_name']
         self._wkbk_formats = configItems['wkbk_formats']
         self._cols_to_remove_basedf = ['field_definition', 'status', 'date_last_changed']
         self._cols_to_keep_uploaded = ['field_definition', 'field_name']
         self._current_date = DateUtils.get_current_date_month_day_year()
         self._document_fields_outputfile_fn = configItems['document_fields_outputfile_fn']
         self._pickle_data_dir = configItems['pickle_data_dir']
+
         self._metadata_config_fn = configItems['metadataset_config']
         self._metadataset_config = ConfigUtils.setConfigs(self._config_dir,  self._metadata_config_fn )
         self._master_dd_config = self._metadataset_config['master_data_dictionary']
-        self._master_dd_fn = self._master_dd_config['json_fn']
+        self._globalfields_config = self._metadataset_config['global_fields']
         self._df_master = MetaDatasets.set_master_df_dd(self._pickle_data_dir, self._master_dd_config['json_fn'])
+        self._globalfields_list = MetaDatasets.set_global_fields_list(self._pickle_data_dir, self._globalfields_config['json_fn'])
         self._datasets_load_list = self.find_attachment_datasets()
-        #print self._datasets_load_list
+        self._cnt_report_columns = ['datasetid', 'dataset_name', 'matched_cnt', 'only_in_master', 'tot_fields_in_attch', 'only_in_attached',  'tot_fields_in_master']
 
     def find_attachment_datasets(self):
       '''gets the datasets with download urls'''
@@ -81,22 +85,6 @@ class ExistingFieldDefs:
         return fnPretty
       return fName
 
-    @staticmethod
-    def get_shts(fn):
-      wkbk = ShtUtils.getWkbk(fn)
-      sht_names = ShtUtils.get_sht_names(wkbk)
-      return {'wkbk': wkbk, 'shts': sht_names}
-
-    @staticmethod
-    def getShtDf(wkbk_stuff, wkbkName, skipRows):
-      '''turns a wksht into a df based on a name and the number of rows to skip'''
-      dfSht = False
-      df = wkbk_stuff['wkbk'].parse(wkbkName, header=skipRows )
-      dfCols = list(df.columns)
-      if len(dfCols) > 3:
-        return df
-      return dfSht
-
 
     def addStatusFields(self, dfAll):
       '''replaces the df definition and data types columns with the xlsx colums'''
@@ -104,97 +92,124 @@ class ExistingFieldDefs:
       dfAll['date_last_changed'] = self._current_date
       return dfAll
 
+    def getAttachmentSheet(self, dataset, fNameXlsx):
+      '''returns of a dataframe of the data dictionary attachment info'''
+      dfSht = None
+      if(FileUtils.getFiles(self._documented_fields_dir+ "xlsx/", fNameXlsx , dataset['attachment_url'])):
+        if(FileUtils.getAttachmentFullPath( self._documented_fields_dir + "xlsx/", fNameXlsx , dataset['attachment_url'])):
+          wkbk_stuff = WkbkUtils.get_shts(self._documented_fields_dir+"xlsx/"+fNameXlsx)
+          wkbkName = self._wkbk_formats['format1']['wksht_name']
+          skipRows = self._wkbk_formats['format1']['skip_rows']
+          try:
+            dfSht = WkbkUtils.getShtDf(wkbk_stuff, wkbkName, skipRows )
+            dfSht = self.formatAttachmentSht(dfSht)
+          except Exception, e:
+            print str(e)
+            print wkbk_stuff
+      return dfSht
+
+    def formatAttachmentSht(self, dfSht):
+      '''formats the dataframe of the attachment sheet so it can be merged with the master data dictionary more easily'''
+      dshts_rename = { k:v for k, v in self._documented_fields_name_mapping.iteritems() if k in  list(dfSht.columns)}
+      dfSht = PandasUtils.renameCols(dfSht, dshts_rename)
+      #just keep the df name and the definition
+      dfSht = dfSht[self._cols_to_keep_uploaded]
+      #convert field name to lower for better match rate
+      if 'field_name' in list(dfSht.columns):
+        dfSht = PandasUtils.colToLower(dfSht, 'field_name')
+      return dfSht
+
+
+    def makeFieldLists(self, dataset, dataset_df, dfSht ):
+      '''tries to match the attachment to the master dd and returns matched and unmatched'''
+      #get fields that match
+      df_both = pd.merge(dataset_df, dfSht, on='field_name')
+      #filter out globals
+      df_both =  df_both[(~df_both['field_name'].isin(self._globalfields_list))] #.reset_index()
+      #filter out just the columns we want to update
+      df_both = self.addStatusFields(df_both)
+      df_both = df_both[self._documented_fields_matched]
+
+      #get the fields that only appear in the master datadict
+      df_only_in_mm = pd.merge(dataset_df, dfSht, how="left", on='field_name')
+      df_only_in_mm = df_only_in_mm.fillna('')
+      df_only_in_mm =  df_only_in_mm[df_only_in_mm['field_definition'] == '']
+      df_only_in_mm =  df_only_in_mm[(~df_only_in_mm['field_name'].isin(self._globalfields_list))] #.reset_index()
+      df_only_in_mm =  df_only_in_mm[self._documented_fields_unmatched]
+
+      #get the fields that are only in attachment
+      df_only_in_attch = pd.merge(dataset_df, dfSht, how="right", on='field_name')
+      df_only_in_attch = df_only_in_attch.fillna('')
+      df_only_in_attch = df_only_in_attch[df_only_in_attch['columnid']== '']
+      df_only_in_attch =  df_only_in_attch[(~df_only_in_attch['field_name'].isin(self._globalfields_list))] #.reset_index()
+      df_only_in_attch =  df_only_in_attch[self._documented_fields_unmatched]
+      df_only_in_attch['datasetid'] = dataset['datasetid']
+      df_only_in_attch['dataset_name'] = dataset['dataset_name']
+      cnt_report = self.getCntsOnFieldMatch(dataset, dataset_df, dfSht, df_both, df_only_in_mm, df_only_in_attch)
+
+      items = {'both': df_both, 'mm': df_only_in_mm, 'attch': df_only_in_attch}
+      dfs_as_rows =  {k:v.to_dict('records') for k,v in items.iteritems() }
+      return cnt_report, dfs_as_rows
+
+    def getCntsOnFieldMatch(self, dataset, dataset_df, dfSht, df_both, df_only_in_master, df_only_in_attachment):
+      '''returns some cnts on match rate-removes globals fields from the counts'''
+      attch_globals = len(dfSht[(dfSht['field_name'].isin(self._globalfields_list))])
+      mm_globals = len(dataset_df[(dataset_df['field_name'].isin(self._globalfields_list))])
+      cnt_report = {'datasetid': dataset['datasetid'],'dataset_name': dataset['dataset_name'],  'tot_fields_in_master': len(dataset_df)-mm_globals, 'tot_fields_in_attch': len(dfSht) - attch_globals, 'matched_cnt': len(df_both) , 'only_in_master': len( df_only_in_master), 'only_in_attached': len(df_only_in_attachment) }
+      cnt_report = {k:str(v) for k,v in cnt_report.iteritems() }
+      return cnt_report
 
     def buildDocumentedFields(self):
       allOthers = []
-      allBlanks = []
-      allDefs = []
+      only_in_mm = []
+      only_in_attch = []
+      in_both = []
       allPdfs = []
+      cnt_report_all =  []
       dowloaded = False
-      for dataset in self._datasets_load_list:
+      for dataset in self._datasets_load_list[0:3]:
         #get the main dataset
-        dataset_id = dataset['datasetid']
+        datasetid = dataset['datasetid']
         dataset_df = self.get_dfDatasetToLoad(dataset['datasetid'])
+        #remove the cols that we will be updating
+        dataset_df = PandasUtils.removeCols(dataset_df, self._cols_to_remove_basedf)
         fNameXlsx = self.make_fnNameXlxs(dataset)
         if( fNameXlsx):
-            print "***xlxs:"+ fNameXlsx + "******"
-            if(FileUtils.getFiles(self._documented_fields_dir+ "xlsx/", fNameXlsx , dataset['attachment_url'])):
-              if(FileUtils.getAttachmentFullPath( self._documented_fields_dir + "xlsx/", fNameXlsx , dataset['attachment_url'])):
-                wkbk_stuff = self.get_shts(self._documented_fields_dir+"xlsx/"+fNameXlsx)
-                wkbkName = self._wkbk_formats['format1']['wksht_name']
-                skipRows = self._wkbk_formats['format1']['skip_rows']
-                try:
-                  dfSht = self.getShtDf(wkbk_stuff, wkbkName, skipRows )
-                except Exception, e:
-                  print str(e)
-                  print wkbk_stuff
-                if dfSht.any:
-                  dshts_rename = { k:v for k, v in self._documented_fields_name_mapping.iteritems() if k in  list(dfSht.columns)}
-                  dfSht = PandasUtils.renameCols(dfSht, dshts_rename)
-                  #just keep the df name and the definition
-                  dfSht = dfSht[self._cols_to_keep_uploaded]
-                  #convert field name to lower for better match rate
-                  if 'field_name' in list(dfSht.columns):
-                    dfSht = PandasUtils.colToLower(dfSht, 'field_name')
-                  #remove the cols that we will be updating
-                  datset_df_clipped = PandasUtils.removeCols(dataset_df, self._cols_to_remove_basedf)
-
-                  df_all = pd.merge(datset_df_clipped, dfSht, how='left', on='field_name')
-                  if dataset_id == "2wsq-7wmv":
-                    print
-                    print "***************"
-                    print "fields in master dd"
-                    print list(datset_df_clipped['field_name'])
-                    print len(datset_df_clipped)
-                    print "fields in sht"
-                    print list(dfSht['field_name'])
-                    print len(dfSht)
-                    print "**match rate*"
-                    match_rate =  ((len(df_all)-len(dfSht))/len(df_all))*100
-                    print match_rate
-                    print "*********"
-
-                  df_all = df_all[df_all['global_field'] == False]
-                  #match_rate =  ((len(df_all)-len(dfSht))/len(df_all))*100
-                  #print match_rate
-                  df_all = df_all[self._documented_fields_list]
-                  df_all = df_all.fillna('')
-                  df_blanks = df_all[df_all['field_definition'] == '']
-                  df_defs = df_all[df_all['field_definition'] != '']
-                  df_defs = self.addStatusFields(df_defs)
-                  blanksList =  df_blanks.to_dict('records')
-                  if dataset_id == "2wsq-7wmv":
-                    print blanksList
-                  defList = df_defs.to_dict('records')
-                  allBlanks  = allBlanks  +  blanksList
-                  allDefs = allDefs + defList
+          print "***xlxs:"+ fNameXlsx + "******"
+          dfSht = self.getAttachmentSheet(dataset, fNameXlsx)
+          if (not(dfSht is None)):
+            cnt_report, dfs_as_rows = self.makeFieldLists( dataset, dataset_df, dfSht)
+            cnt_report_all.append(cnt_report)
+            in_both  = in_both + dfs_as_rows['both']
+            only_in_mm = only_in_mm  + dfs_as_rows['mm']
+            only_in_attch = only_in_attch + dfs_as_rows['attch']
+            print cnt_report
+          else:
+            dataset['file_name'] = fNameXlsx
+            allOthers.append(dataset)
         else:
             fNamePdf = self.make_fnNamePdf(dataset)
             if(fNamePdf):
               if(FileUtils.getFiles(self._documented_fields_dir+ "pdf/", fNamePdf , dataset['attachment_url'])) :
-
                 print "***pdf:"+ fNamePdf + "****"
-                try:
-                    allPdfs.append(dataset)
-
-                except Exception, e:
-                  print str(e)
+                dataset['file_name'] = fNamePdf
+                allPdfs.append(dataset)
             else:
               fnameOther = self.make_fnNameOther(dataset)
               if(FileUtils.getFiles(self._documented_fields_dir+ "other/", fnameOther , dataset['attachment_url'])) :
                 print "********other*******"
-                print
-                print
-                print dataset
+                print fnameOther
+                dataset['file_name'] = fnameOther
+                allOthers.append(dataset)
                 print "********************"
-      #print allFields
-      wroteFileDefs = FileUtils.write_wkbk_csv(self._documented_fields_dir +"output/" + self._document_fields_outputfile_fn, allDefs, self._documented_fields_list)
-      wroteFileBlanks =  FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "blank_fields.csv", allBlanks, self._documented_fields_list)
-      wrotePdfDatasets = FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "pdf_datasets.csv", allPdfs,self._pdf_others_fields_list)
 
-      print wroteFileDefs
-      print wroteFileBlanks
-      print wrotePdfDatasets
+      wroteFileDefs = FileUtils.write_wkbk_csv(self._documented_fields_dir +"output/" + self._document_fields_outputfile_fn, in_both, self._documented_fields_matched)
+      wroteFileOnlyInAttach =  FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "fields_only_in_attachments.csv", only_in_attch,  self._documented_fields_unmatched)
+      wroteFileOnlyInMaster =  FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "fields_only_in_master_dd.csv", only_in_mm, self._documented_fields_unmatched)
+      wrotePdfDatasets = FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "pdf_datasets.csv", allPdfs,self._pdf_others_fields_list)
+      wroteOthersDatasets = FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "other_datasets.csv", allOthers,self._pdf_others_fields_list)
+      wroteCntReport = FileUtils.write_wkbk_csv(self._documented_fields_dir + "output/"+ "match_report.csv",cnt_report_all, self._cnt_report_columns)
+      return wroteFileDefs, wroteFileOnlyInAttach, wroteFileOnlyInMaster, wrotePdfDatasets, wroteOthersDatasets
 
 
 
