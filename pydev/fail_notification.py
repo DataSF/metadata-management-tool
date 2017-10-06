ea20c616e089fad2f8021afb00effd89e17c53a5
j9recurses <janine.heiser@gmail.com>
2017-10-06 13:02:56 -0700

    adds more changes to consolidation

--- pydev/fail_notification.py
+++ pydev/fail_notification.py
@@ -1,20 +1,63 @@
- 
-from Emailer import *
-from DictUtils import *
+# coding: utf-8
+#!/usr/bin/env python
 
 
+from optparse import OptionParser
+import sys
+from ConfigUtils import *
+from PyLogger import *
+from JobStatusEmailerComposer import *
 
 
- e = Emailer(self.configItems)
-        emailconfigs = e.setConfigs()
-        if((os.path.isfile(self.logfile_fullpath)) and (msg_attachments is None)):
-            e.sendEmails( subject_line, msgBody, self.logfile_fname, self.logfile_fullpath)
-        elif msg_attachments:
-            e.sendEmails( subject_line, msgBody, None, None, None, msg_attachments)
-        else:
-            e.sendEmails( subject_line, msgBody)
-        logger_msg =  "****JOB STATUS: " +  self.job_name + "*:* "+ subject_line +"***"
-        print logger_msg
-        print "Email Sent!"
-        self._logger.info(logger_msg)
-        self._logger.info( subject_line)
+def parse_opts():
+  sys.setdefaultencoding('utf8')
+  helpmsgConfigFile = 'Use the -c to add a config yaml file. EX: fieldConfig.yaml'
+  parser = OptionParser(usage='usage: %prog [options] ')
+  parser.add_option('-c', '--configfile',
+                      action='store',
+                      dest='configFn',
+                      default=None,
+                      help=helpmsgConfigFile ,)
+
+  helpmsgConfigDir = 'Use the -d to add directory path for the config files. EX: /home/ubuntu/configs'
+  parser.add_option('-d', '--configdir',
+                      action='store',
+                      dest='configDir',
+                      default=None,
+                      help=helpmsgConfigDir ,)
+  helpmsgConfigDir = 'Use the -m to add a fail notication messge. EX: "Failed: This job failed"'
+  parser.add_option('-m', '--fail_msg',
+                      action='store',
+                      dest='fail_msg',
+                      default=None,
+                      help=helpmsgConfigDir ,)
+
+  (options, args) = parser.parse_args()
+  if  options.configFn is None:
+    print "ERROR: You must specify a config yaml file!"
+    print helpmsgConfigFile
+    exit(1)
+  elif options.configDir is None:
+    print "ERROR: You must specify a directory path for the config files!"
+    print helpmsgConfigDir
+    exit(1)
+  fieldConfigFile = options.configFn
+  config_inputdir = options.configDir
+  fail_msg = options.fail_msg
+  return config_inputdir, fieldConfigFile, fail_msg
+
+
+def main():
+  reload(sys)
+  config_inputdir, fieldConfigFile, fail_msg = parse_opts()
+  configItems = ConfigUtils.setConfigs(config_inputdir, fieldConfigFile )
+  configItems['job_name'] = fail_msg
+  lg = pyLogger(configItems)
+  logger = lg.setConfig()
+  dsse = JobStatusEmailerComposer(configItems, logger)
+  dataset_info = {'Socrata Dataset Name': fail_msg, 'SrcRecordsCnt':0, 'DatasetRecordsCnt':0, 'fourXFour': "Job Failed"}
+  dataset_info['isLoaded'] = 'failed'
+  dsse.sendJobStatusEmail([dataset_info])
+
+if __name__ == "__main__":
+    main()
--- pydev/push_public_version_of_master_dd.py
+++ pydev/push_public_version_of_master_dd.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+#!/usr/bin/env python
+
 import sys
 from Utils import *
 from optparse import OptionParser
