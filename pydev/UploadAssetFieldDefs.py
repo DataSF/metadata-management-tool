c593bd2f917c1ea71c7dad88bf01af8d867b6833
j9recurses <janine.heiser@gmail.com>
2018-01-30 09:43:10 -0800

    fixes issues with existing definitions

--- pydev/UploadAssetFieldDefs.py
+++ pydev/UploadAssetFieldDefs.py
@@ -18,7 +18,6 @@ class UploadAssetFieldDefs:
         self._pickle_data_dir = configItems['pickle_data_dir']
         self._pickle_dir = configItems['pickle_dir']
         self._metadata_config_fn = configItems['metadataset_config']
-
         self._metadataset_config = ConfigUtils.setConfigs(self._config_dir,  self._metadata_config_fn )
         self._master_dd_config = self._metadataset_config['master_data_dictionary']
         self._globalfields_config = self._metadataset_config['global_fields']
@@ -27,7 +26,7 @@ class UploadAssetFieldDefs:
         #self._globalfields_list = MetaDatasets.set_global_fields_list(self._pickle_data_dir, self._globalfields_config['json_fn'])
         self._asset_fields_df = MetaDatasets.get_metadata_df(self._pickle_data_dir, self._assetfields_config['json_fn'])
         self._valsToNotOverride =  ['Complete', "Submitted by Coordinator" , "Submitted by Steward"]
-        self._keys_to_keep = ['columnid', 'field_definition', 'status', 'date_last_changed']
+        self._keys_to_keep = ['columnid', 'field_definition', 'status', 'date_last_changed', 'field_documented']
         self._current_date = DateUtils.get_current_date_month_day_year()
         self._output_file = configItems['output_json_fn']
         self.json_key = configItems['json_key']
@@ -49,6 +48,10 @@ class UploadAssetFieldDefs:
 
     def set_status(self, df):
         '''sets the status'''
+        def isComplete(row):
+            if row['field_definition'] != '':
+                return True
+        df['field_documented'] =  df.apply(lambda row: isComplete(row), axis=1)
         df['status'] = 'Entered on Portal'
         df['date_last_changed'] = self._current_date
         return df
@@ -61,13 +64,16 @@ class UploadAssetFieldDefs:
         df_master_removed_or_private = df_master_removed_or_private[['columnid', 'privateordeleted']]
         return df_master_removed_or_private
 
+
     def getFieldDefList(self):
         filtered = self.filter_dfs()
         wroteJsonFile = False
         if filtered:
             df_asset_field_defs = pd.merge(self._df_master, self._asset_fields_df_filtered, on='columnid')
             df_asset_field_defs = self.set_status(df_asset_field_defs)
+
             df_asset_field_defs = df_asset_field_defs[self._keys_to_keep]
+
             asset_field_defs = PandasUtils.convertDfToDictrows(df_asset_field_defs)
             df_removed_or_private = self.get_deleted_or_private_fields()
             removed_or_private =  PandasUtils.convertDfToDictrows(df_removed_or_private)
@@ -78,3 +84,4 @@ class UploadAssetFieldDefs:
                 print "could not wrtite file"
                 print str(e)
         return wroteJsonFile
+ df.apply(lambda row: get_nbe_id(row, socrataQueriesObject), axis=1)
