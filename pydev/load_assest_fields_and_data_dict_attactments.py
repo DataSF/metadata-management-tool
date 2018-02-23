a401a50eaae6e695582d576cf4632892fa8f1c02
j9recurses <janine.heiser@gmail.com>
2018-02-22 16:48:53 -0800

    testing bash script in stages

--- pydev/load_assest_fields_and_data_dict_attactments.py
+++ pydev/load_assest_fields_and_data_dict_attactments.py
@@ -71,11 +71,12 @@ def main():
   q2 = "?$select=systemid, dataset_name group by systemid, dataset_name"
   results_json2 =  sqry.getQry('skzx-6gkn', q2)
   df_asset_fields = BuildDatasets.makeDf(results_json)
+  df_merged = df_asset_inventory.merge(df_asset_fields, how='left', on='datasetid')
+  print df_merged
 
-
-  logger.info(finshed_datasets)
-  dsse = JobStatusEmailerComposer(configItems, logger)
-  dsse.sendJobStatusEmail(finshed_datasets)
+  #logger.info(finshed_datasets)
+  #dsse = JobStatusEmailerComposer(configItems, logger)
+  #dsse.sendJobStatusEmail(finshed_datasets)
 
 if __name__ == "__main__":
     main()
